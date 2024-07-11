841841fd07115cbade864542d0742c493acb2b11
sajeewan1503 <sathiyanathansajeewan3@gmail.com>
2024-07-11 09:40:51 +0530

    Creted new Project Javascript Programming

---
+++ demo.js
@@ -0,0 +1,101 @@
+/*var a = 10
+var b=30
+console.log(a+b)*/
+
+/*function abc(){
+    console.log("Hello")
+}
+abc()*/
+
+/*function samsung() {
+  console.log("This is samsung phone");
+}
+function Iphone() {
+  console.log("This is Iphone phone");
+}
+function Redmi() {
+  console.log("This is Redmi phone");
+}
+function Oppo() {
+  console.log("This is Oppo phone");
+}
+samsung();
+Iphone();
+Redmi();
+Oppo();
+*/
+
+/*var a = 20;
+var b = 30;
+
+function add() {
+  console.log((a + b));
+}
+
+add();*/
+
+/*var factor = "Vijay";
+var fplayer = "Virat Kohli";
+var fmovie = "Naam";
+
+function Favourite() {
+  console.log("Favourite Actor:" + factor);
+  console.log("Favourite Player:" + fplayer);
+  console.log("Favourite Movie:" + fmovie);
+}
+
+Favourite();*/
+
+/*function area(l,b){
+    var a = l*b
+    console.log("Area is:" +a)
+}
+
+var length=10
+var breadth=20
+
+area (10,20)*/
+
+/*var rain = false;
+if (rain) {
+  console.log("take an umbrella");
+} else {
+  console.log("Enjoy the sunshine");
+}*/
+
+/*var homework = true;
+if (homework) {
+  console.log("Great job!");
+} else {
+  console.log("Finish your homework before playing");
+}*/
+
+/*var cookies = true;
+if (cookies) {
+  console.log("Would you like cookies?");
+} else {
+  console.log("Time to bake more cookies");
+}*/
+
+/*var color = "green";
+
+if (color == "red") {
+  console.log("Stop");
+}
+if (color == "yellow") {
+  console.log("Get Ready");
+}
+if (color == "green") {
+  console.log("Go");
+}*/
+
+//FOR LOOP//
+/*for (count = 1; count <= 5; count = count + 1) {
+  console.log("John");
+}*/
+
+/*for (count = 1; count <= 10; count = count + 2) {
+  console.log(count);
+}*/
+
+//ARRAYS AND OBJECTS//
